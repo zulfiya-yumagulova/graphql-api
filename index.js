091bd2eed6be037e7d36654cb1c791cc3b58ba34
Zulfiya Yumagulova <83015952+zulfiya-yumagulova@users.noreply.github.com>
2025-02-20 16:28:02 +0000

    add resolvers

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./db.js";
+
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    reviews: () => db.reviews,
+    authors: () => db.authors,
+  },
+};
 
 // Server set up
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: {
